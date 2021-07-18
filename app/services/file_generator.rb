618abb39959994619129049d615476e1d47ab9c6
Flip437 <philippediollot@hotmail.fr>
2021-07-18 13:12:19 +0200

    fixing average speed function issues

--- app/services/file_generator.rb
+++ app/services/file_generator.rb
@@ -28,6 +28,8 @@ class FileGenerator
     return exported_file
   end
 
+
+
   def create_temp_file(model)
     filename = model.file.blob.filename
     @tmp = Tempfile.new([filename.base, filename.extension_with_delimiter], binmode: true)
@@ -35,6 +37,8 @@ class FileGenerator
     @tmp.rewind
     @tmp
   end
+
+
 
   def csv_open(sheet, col_index_array, tmp_csv, headers, first_row, empty_cols_array, headers_row, class_cat_array, class_sex_array)
     CSV.open(tmp_csv.path, "a+", col_sep: ";", headers: true) do |new_csv_row|
@@ -69,9 +73,10 @@ class FileGenerator
   end
 
 
+
   def format_csv_row(row, headers)
     headers.each_with_index do |cell, index|
-      return if row[index].blank?
+      next if row[index].blank?
       case cell
       when "Doss." || "Dossard"
         row[index] = row[index].delete('^0-9')
@@ -85,9 +90,7 @@ class FileGenerator
       when "Sexe"
         row[index] = row[index].gsub('Femme', 'F').gsub('Homme', 'H')
       when "Temps"
-        row[index] = '0' + row[index] if row[index][1] == 'h' || row[index][1] == ':'
-        row[index] = '00:' + row[index] if row[index][2] == ":" && row[index].length == 5
-        row[index] = row[index].gsub(',00', '').gsub('sec', '').gsub('h', ':').gsub('m', ':')
+        row[index] = convert_time(row[index])
       when "Distance" || "Distance épreuve"
         row[index] = row[index].to_s.gsub('km', '')
         row[index] = row[index].to_i / 1000 if row[index].to_i > 500
@@ -96,14 +99,15 @@ class FileGenerator
   end
 
 
+
   def class_cat_calculate(sheet, headers_row, first_row)
     class_array = []
     class_cat_array = []
-    cat_col_index = sheet.row(headers_row).index('Catégorie')
-    # ["Catégorie"].each do |str|
-    #   index = sheet.row(headers_row).index(str)
-    #   cat_col_index = index if !index.nil?
-    # end
+    cat_col_index = nil
+    ["Catégorie", "Cat"].each do |str|
+      index = sheet.row(headers_row).index(str)
+      cat_col_index = index if !index.nil?
+    end
 
     return if cat_col_index.nil?
     cat_col = sheet.column(cat_col_index + 1)
@@ -116,10 +120,12 @@ class FileGenerator
     return class_cat_array
   end
 
+
+
   def class_sex_calculate(sheet, headers_row, first_row)
     class_array = []
     class_sex_array = []
-    sex_col_index = sheet.row(headers_row).index('Sx') || sex_col_index = sheet.row(headers_row).index('SEXE')
+    sex_col_index = nil
 
     ['Sx', 'SEXE'].each do |str|
       index = sheet.row(headers_row).index(str)
@@ -137,19 +143,26 @@ class FileGenerator
     return class_sex_array
   end
 
+
+
   def add_class_cat(csv_row, class_cat_array, row_index, headers)
     csv_row[3] = class_cat_array[row_index] if !class_cat_array.nil? && headers == "freshstart_headers" #if clas cat non présent
     csv_row[15] = class_cat_array[row_index] if !class_cat_array.nil? && headers == "fftri_headers" #if clas cat non présent
   end
+
+
 
   def add_class_sex(csv_row, class_sex_array, row_index, headers)
     csv_row[2] = class_sex_array[row_index] if !class_sex_array.nil? && headers == "freshstart_headers" #if clas cat non présent
     csv_row[16] = class_sex_array[row_index] if !class_sex_array.nil? && headers == "fftri_headers" #if clas cat non présent
   end
 
-  def average_speed_calculate(sheet, csv_row, headers_row, xl_row, headers)
-    distance_col_index = 9999
-    time_col_index = 9999
+
+
+  def average_speed_calculate(sheet, csv_row, headers_row, xl_row, headers)#, time_design_array, speed_design_array)
+    distance_col_index = nil
+    time_col_index = nil
+
     ['Distance', 'Dist'].each do |str|
       index = sheet.row(headers_row).index(str)
       distance_col_index = index if !index.nil?
@@ -160,29 +173,43 @@ class FileGenerator
       time_col_index = index if !index.nil?
     end
 
-    puts "INDEXSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
-    puts time_col_index
-    puts distance_col_index
-    puts sheet.cell(xl_row, distance_col_index+1)
-    puts sheet.cell(xl_row, time_col_index+1)
-    time = sheet.cell(xl_row, time_col_index+1)
+    if distance_col_index.nil? || time_col_index.nil?
+      return csv_row[10] = 'Average not processable'
+    elsif  sheet.cell(xl_row, distance_col_index + 1).nil? ||sheet.cell(xl_row, distance_col_index + 1).blank?
+      return csv_row[10] = 'Average not processable'
+    elsif  sheet.cell(xl_row, time_col_index + 1).nil? ||sheet.cell(xl_row, time_col_index + 1).blank?
+      return csv_row[10] = 'Average not processable'
+    end
 
+    distance = convert_dist(sheet.cell(xl_row, distance_col_index + 1))
+    time = convert_time(sheet.cell(xl_row, time_col_index + 1))
+
+    hours = time[0..1]
+    minutes = time[3..4]
+    seconds = time[6, time.length]
+    hour_time = (hours.to_i * 3600 + minutes.to_i * 60 + seconds.to_f) / 3600
+    average_speed = (distance / hour_time).round(2)
+
+    return csv_row[10] = average_speed if headers == "freshstart_headers" and !distance_col_index.nil? && !time_col_index.nil?
+  end
+
+
+  
+  def convert_time(time)
     time = '0' + time if time[1] == 'h' || time[1] == ':'
     time = '00:' + time if time[2] == ":" && time.length == 5
     time = time.gsub(',00', '').gsub('sec', '').gsub('h', ':').gsub('m', ':')
-
-    puts time
-
-
-
-    puts "INDEXSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
-
-    return csv_row[10] = 'Average not processable' if distance_col_index.nil? || time_col_index.nil?
-
-    csv_row[10] = sheet.cell(xl_row, distance_col_index + 1) / sheet.cell(xl_row, time_col_index + 1) if headers == "freshstart_headers"
-
-
-    
+    return time
   end
+
+
+
+  def convert_dist(dist)
+    dist = dist.to_s.gsub('km', '')
+    dist.to_i > 500 ? dist = dist.to_i / 1000 : dist = dist.to_i
+    return dist
+  end
+
+
 
 end
